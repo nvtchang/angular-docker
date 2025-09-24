f0af17ca610250763d8750dc8aaf319e5dfd1f61
trangngo-k <trang.ngo@kenect.com>
2025-09-24 22:27:33 +0700

    init new app

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from './navigation/navigation.component'; 
+import { DashboardComponent } from './dashboard/dashboard.component'; 
+
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, NavigationComponent, DashboardComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+
+export class AppComponent {
+  title = 'angular-app-docker';
+}
